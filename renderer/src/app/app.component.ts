7c1ffbe6cf2edf718a3d75936dc99c4df0b2e2f5
Seokju Na <seokju.me@gmail.com>
2017-08-10 17:19:59 +0900

    Redesign app

--- renderer/src/app/app.component.ts
+++ renderer/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
+import { ToolItem, ToolItemInterface } from './ui/tool-bar/tool-bar';
 import { ClassName } from './utils/class-name';
 
 
@@ -9,17 +11,39 @@ import { ClassName } from './utils/class-name';
 })
 export class AppComponent implements OnInit {
     asideOpened = false;
+    activityViewNavigators: ToolItemInterface[];
+    globalActions: ToolItemInterface[];
+    noteActions: ToolItemInterface[];
     cn = new ClassName('App');
 
     constructor() {
     }
 
     ngOnInit() {
+        this.activityViewNavigators = [
+            { title: 'Finder(⌘⇧E)' , iconName: 'folder' },
+            { title: 'SCM(⌘⇧G)', iconName: 'git-square' }
+        ];
+
+        this.globalActions = [
+            { title: 'Setting', iconName: 'cog' }
+        ];
+
+        this.noteActions = [
+            { title: 'Change note view', iconName: 'eye' },
+            { title: 'Discard note', iconName: 'trash' },
+            { title: 'Export note', iconName: 'external-link' }
+        ];
+
         this.parseClassName();
     }
 
     private parseClassName() {
         this.cn.setModifier('aside', this.asideOpened ? 'opened' : 'closed');
+    }
+
+    handleNoteAction(noteAction: ToolItem) {
+        console.log(noteAction);
     }
 
     toggleAside() {
--- renderer/src/app/app.module.ts
+++ renderer/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CoreModule } from './core/core.module';
 import { UIModule } from './ui/ui.module';
 import { NoteModule } from './note/note.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
     imports: [
