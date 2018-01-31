be8b87f05b3e31bb6ca52aaa8ec1d0b03ead91b8
Seokju Na <seokju.me@gmail.com>
2018-01-31 14:13:07 +0900

    Add button ui spec

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { BemBlock } from '../../../common/bem-class';
 
 
 @Component({
     selector: 'gd-button',
     templateUrl: './button.component.html',
-    styleUrls: ['./button.component.less']
+    styleUrls: ['./button.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnChanges {
+    @Input() type = 'normal';
+    @Input() size = 'regular';
+    @Input() disabled = false;
+
+    className = new BemBlock('Button');
+
+    ngOnChanges(): void {
+        this.className
+            .setModifier('type', this.type)
+            .setModifier('size', this.size);
+    }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule
     ],
     declarations: [
         ButtonComponent
     ],
     exports: [
         CommonModule,
+        ReactiveFormsModule,
+
         ButtonComponent
     ]
 })
