14adf3bd877cf8d236cf9516ced0257aad2e73f6
Seokju Na <seokju.me@gmail.com>
2018-01-31 14:14:07 +0900

    Add icon ui spec

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from './button/button.component';
+import { IconComponent } from './icon/icon.component';
 
 
 @NgModule({
@@ -10,13 +11,18 @@ import { ButtonComponent } from './button/button.component';
         ReactiveFormsModule
     ],
     declarations: [
-        ButtonComponent
+        // Button
+        ButtonComponent,
+        // Icon
+        IconComponent
     ],
     exports: [
         CommonModule,
         ReactiveFormsModule,
-
-        ButtonComponent
+        // Button
+        ButtonComponent,
+        // Icon
+        IconComponent
     ]
 })
 export class SharedModule {
