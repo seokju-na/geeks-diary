e3291b178b83fb998c98d10a4efac11144a6a440
seokju-na <seokju.me@gmail.com>
2018-02-09 22:49:12 +0900

    Add button component ui
    Took 1 minute

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
 import { BemBlock } from '../../../common/bem-class';
 
 
@@ -8,14 +8,22 @@ import { BemBlock } from '../../../common/bem-class';
     styleUrls: ['./button.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonComponent implements OnChanges {
+export class ButtonComponent implements OnInit, OnChanges {
     @Input() type = 'normal';
     @Input() size = 'regular';
     @Input() disabled = false;
 
     className = new BemBlock('Button');
 
+    ngOnInit(): void {
+        this.parseClassName();
+    }
+
     ngOnChanges(): void {
+        this.parseClassName();
+    }
+
+    private parseClassName(): void {
         this.className
             .setModifier('type', this.type)
             .setModifier('size', this.size);
