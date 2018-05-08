f76954f345d8c9af34bc54b99834b1ad21b4098c
seokju-na <seokju.me@gmail.com>
2018-05-09 03:54:29 +0900

    Change code sequences
    Took 1 minute

--- src/spec.js
+++ src/spec.js
@@ -1,3 +1,6 @@
+// Source from
+// https://github.com/colinskow/angular-electron-dream-starter/blob/master/config/spec-bundle.js
+
 Error.stackTraceLimit = Infinity;
 
 require('core-js/es7/reflect');
@@ -10,33 +13,17 @@ require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 
-
-const fs = require('fs');
-
-try {
-    fs.accessSync('temp/');
-} catch (err) {
-    fs.mkdirSync('temp/');
-}
-
+const fse = require('fs-extra');
 
 const testing = require('@angular/core/testing');
 const browser = require('@angular/platform-browser-dynamic/testing');
+
 
 testing.TestBed.initTestEnvironment(
     browser.BrowserDynamicTestingModule,
     browser.platformBrowserDynamicTesting()
 );
 
-/*
- * Ok, this is kinda crazy. We can use the context method on
- * require that webpack created in order to tell webpack
- * what files we actually want to require or import.
- * Below, context will be a function/object with file names as keys.
- * Using that regex we are saying look in ../src then find
- * any file that ends with spec.ts and get its path. By passing in true
- * we say do this recursively
- */
 
 if (window.MONACO) {
     runTest();
@@ -48,6 +35,8 @@ if (window.MONACO) {
 
 
 function runTest() {
+    fse.ensureDirSync('temp/');
+
     const testContext = require.context('./app', true, /\.spec\.ts/);
     testContext.keys().map(testContext);
 }
