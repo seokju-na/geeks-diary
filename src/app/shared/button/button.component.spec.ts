53428728d094db872bca8e723a07de05ba7f52bb
seokju-na <seokju.me@gmail.com>
2018-06-29 19:57:09 +0900

    Remove spinner option
    Took 11 hours 26 minutes

--- src/app/shared/button/button.component.spec.ts
+++ src/app/shared/button/button.component.spec.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { expectDebugElement } from '../../../testing/validation';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { ButtonComponent } from './button.component';
 
@@ -43,25 +42,13 @@ describe('app.shared.button', () => {
         expect(button.componentInstance.size).toEqual('regular');
         expect(button.nativeElement.classList.contains('Button--size-regular')).toBeTruthy();
     });
-
-    it('should show spinner when input value changes', () => {
-        component.someTaskPending = true;
-        fixture.detectChanges();
-
-        const button = fixture.debugElement.query(By.directive(ButtonComponent));
-        const spinner = button.query(By.directive(SpinnerComponent));
-
-        expect(spinner.componentInstance.show).toBeTruthy();
-        expectDebugElement(button).toContainsClass('Button--showSpinner');
-    });
 });
 
 
 @Component({
     template: `
-        <button gd-button [showSpinner]="someTaskPending">Normal content</button>
+        <button gd-button>Normal content</button>
     `,
 })
 class TestButtonComponent {
-    someTaskPending = false;
 }
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,3 @@
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import {
     ChangeDetectionStrategy,
     Component,
@@ -19,26 +18,8 @@ import {
 })
 export class ButtonComponent implements OnInit {
     @HostBinding('class.Button') private buttonClassName = true;
-
-    @Input()
-    get showSpinner() { return this._showSpinner; }
-
-    set showSpinner(value: boolean) {
-        this._showSpinner = coerceBooleanProperty(value);
-
-        const host = this.elementRef.nativeElement;
-
-        if (this._showSpinner) {
-            host.classList.add('Button--showSpinner');
-        } else {
-            host.classList.remove('Button--showSpinner');
-        }
-    }
-
     @Input() buttonType = 'normal';
     @Input() size = 'regular';
-
-    private _showSpinner = false;
 
     constructor(private elementRef: ElementRef) {
     }
