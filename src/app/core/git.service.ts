86833e3689685ac7b2ea2810b355037b62261bfb
seokju-na <seokju.me@gmail.com>
2018-06-29 20:04:48 +0900

    Add git service
    Took 25 seconds

---
+++ src/app/core/git.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NgZone } from '@angular/core';
+import { remote } from 'electron';
+import { Repository, StatusFile } from 'nodegit';
+import { Observable } from 'rxjs';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { of } from 'rxjs/observable/of';
+import { catchError, mapTo } from 'rxjs/operators';
+import { IpcChannelClient } from '../../common/ipc-channel';
+import { enterZone } from '../../common/rx-helpers';
+
+
+@Injectable()
+export class GitService {
+    private readonly nodeGit = remote.getGlobal('nodegit');
+    private readonly ipc = new IpcChannelClient('git');
+
+    constructor(private ngZone: NgZone) {
+    }
+
+    isRepositoryExists(dirPath: string): Observable<boolean> {
+        return this.openRepository(dirPath).pipe(
+            mapTo(true),
+            catchError(() => of(false)),
+        );
+    }
+
+    createRepository(dirPath: string): Observable<Repository> {
+        return fromPromise(this.nodeGit.Repository.init(dirPath, 0))
+            .pipe(enterZone<Repository>(this.ngZone));
+    }
+
+    openRepository(dirPath: string): Observable<Repository> {
+        return fromPromise(this.nodeGit.Repository.open(dirPath))
+            .pipe(enterZone<Repository>(this.ngZone));
+    }
+
+    clone(remoteUrl: string, dirPath: string): Observable<Repository> {
+        return fromPromise(this.ipc.request('clone', { remoteUrl, dirPath }))
+            .pipe(enterZone<Repository>(this.ngZone));
+    }
+
+    getFileStatues(repository: Repository): Observable<StatusFile[]> {
+        return fromPromise(repository.getStatus()).pipe(enterZone<StatusFile[]>(this.ngZone));
+    }
+}
