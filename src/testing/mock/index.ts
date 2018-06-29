1541818d8b1e8006c050716a8147e580eec11fa0
seokju-na <seokju.me@gmail.com>
2018-06-29 20:05:58 +0900

    Add mock git service
    Took 24 seconds

--- src/testing/mock/index.ts
+++ src/testing/mock/index.ts
@@ -2,4 +2,5 @@ export * from './mock-actions';
 export * from './mock-dialog';
 export * from './mock-dialog-ref';
 export * from './mock-fs.service';
+export * from './mock-git.service';
 export * from './mock-ng-zone';
