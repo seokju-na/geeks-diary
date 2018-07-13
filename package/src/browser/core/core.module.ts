3f0b3f32f1afc7e117c9f7a8a6980524dcc9b02c
seokju-na <seokju.me@gmail.com>
2018-07-13 18:22:20 +0900

    Add workspace database provider

--- package/src/browser/core/core.module.ts
+++ package/src/browser/core/core.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
+import { UIModule } from '../ui/ui.module';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { WorkspaceDatabaseProvider } from './workspace-database';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 
 @NgModule({
-    imports: [],
+    imports: [
+        UIModule,
+    ],
     declarations: [
         TitleBarComponent,
+        ConfirmDialogComponent,
+    ],
+    entryComponents: [
+        ConfirmDialogComponent,
+    ],
+    providers: [
+        WorkspaceDatabaseProvider,
     ],
     exports: [
         TitleBarComponent,
+        ConfirmDialogComponent,
     ],
 })
 export class CoreModule {
