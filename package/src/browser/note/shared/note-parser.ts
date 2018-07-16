8511f9299ca1af9dc1ac488c80ca6f1c2cb9812c
seokju-na <seokju.me@gmail.com>
2018-07-16 22:23:17 +0900

    Add note parser test cases and implementation
    * Add fixtures for testing raw content value of note.
    * Add 'remarkable', 'remarkable-meta' dependencies for parsing markdown file.
    * Add 'get-md-title' dependency for parsing title of note from markdown file.

--- package/src/browser/note/shared/note-parser.ts
+++ package/src/browser/note/shared/note-parser.ts
@@ -1,45 +1,149 @@
-// import { Injectable } from '@angular/core';
-// import * as path from 'path';
-// import { Observable } from 'rxjs';
-// import { Note } from '../../../models/note';
-// import { FsService } from '../../core/fs.service';
-// import { WorkspaceService } from '../../core/workspace.service';
-// import { NoteContent } from './note-content.model';
-// import { NoteItem } from './note-item.model';
-//
-//
-// @Injectable({
-//     providedIn: 'root',
-// })
-// export class NoteParser {
-//     constructor(
-//         private fs: FsService,
-//         private workspace: WorkspaceService,
-//     ) {
-//     }
-//
-//     generateNoteForUnrecordedContentFile(contentFileName: string): Observable<void> {
-//         const now = new Date().getTime();
-//         const newNote: Note = {
-//             id: 'uniqueId',
-//             title: path.basename(contentFileName, '.md'),
-//             snippets: [],
-//             stackIds: [],
-//             contentFileName,
-//             contentFilePath: this.resolveContentFilePath(contentFileName),
-//             createdDatetime: now,
-//             updatedDatetime: now,
-//         };
-//
-//         // Save at 'geeks-diary.json'.
-//         return null;
-//     }
-//
-//     loadNoteContent(note: NoteItem): Observable<NoteContent> {
-//         return null;
-//     }
-//
-//     private resolveContentFilePath(contentFileName: string): string {
-//         return path.resolve(this.workspace.workspaceDirPath, contentFileName);
-//     }
-// }
+import { Injectable } from '@angular/core';
+import * as os from 'os';
+import { Note } from '../../../models/note';
+import { NoteSnippetTypes } from '../../../models/note-snippet';
+import { NoteModule } from '../note.module';
+import { NoteContent, NoteSnippetContent } from './note-content.model';
+import getMdTitle = require('get-md-title');
+import Remarkable = require('remarkable');
+
+
+interface NoteContentRawValueParseResult {
+    title: string;
+    stackIds: string[];
+    snippets: NoteSnippetContent[];
+    createdDatetime?: number;
+}
+
+
+const md = new Remarkable();
+md.use(require('remarkable-meta'));
+
+
+@Injectable({
+    providedIn: NoteModule,
+})
+export class NoteParser {
+    generateNoteContent(note: Note, contentRawValue: string): NoteContent {
+        if (!contentRawValue) {
+            return null;
+        }
+
+        const lines = contentRawValue.split(os.EOL);
+        const snippets: NoteSnippetContent[] = [];
+
+        for (const snippet of note.snippets) {
+            const startIndex = snippet.startLineNumber - 1;
+            const endIndex = snippet.endLineNumber - 1;
+
+            const value = lines.slice(startIndex, endIndex + 1).join(os.EOL);
+
+            snippets.push({
+                ...snippet,
+                value,
+            });
+        }
+
+        return {
+            noteId: note.id,
+            snippets,
+        };
+    }
+
+    private _parseTokens = function* parseLine(
+        lines: string[],
+        _tokens: Remarkable.Token[],
+    ): IterableIterator<NoteSnippetContent> {
+        let start = 0;
+        let end = 0;
+        let startLine;
+        let endLine;
+
+        const tokens = _tokens.filter(token =>
+            !!token.lines && !!(<any>token).content);
+
+        for (let i = 0; i < tokens.length; i++) {
+            if (tokens[i].type === 'fence') {
+                if (start !== i && end !== i) {
+                    startLine = tokens[start].lines[0];
+                    endLine = tokens[end].lines[1];
+
+                    yield {
+                        type: NoteSnippetTypes.TEXT,
+                        value: lines.slice(startLine, endLine).join('\n'),
+                        startLineNumber: startLine + 1,
+                        endLineNumber: endLine,
+                    };
+                }
+
+                [startLine, endLine] = tokens[i].lines;
+
+                yield {
+                    type: NoteSnippetTypes.CODE,
+                    value: lines.slice(...tokens[i].lines).join('\n'),
+                    startLineNumber: startLine + 1,
+                    endLineNumber: endLine,
+                    codeLanguageId: (<any>tokens[i]).params,
+                    codeFileName: '',
+                };
+
+                start = i + 1;
+                end = i + 1;
+            } else {
+                end = i;
+
+                if (i === tokens.length - 1) {
+                    startLine = tokens[start].lines[0];
+                    endLine = tokens[end].lines[1];
+
+                    yield {
+                        type: NoteSnippetTypes.TEXT,
+                        value: lines.slice(startLine, endLine).join('\n'),
+                        startLineNumber: startLine + 1,
+                        endLineNumber: endLine,
+                    };
+                }
+            }
+        }
+    };
+
+    parseNoteContentRawValue(contentRawValue: string): NoteContentRawValueParseResult {
+        const snippets: NoteSnippetContent[] = [];
+        const lines = contentRawValue.split('\n');
+        const tokens = md.parse(contentRawValue, { breaks: true });
+        const metadata = (<any>md).meta;
+
+        for (const snippet of this._parseTokens(lines, tokens)) {
+            snippets.push(snippet);
+        }
+
+        const firstHeaderTitle = getMdTitle(contentRawValue);
+        const title = metadata.title
+            ? metadata.title
+            : firstHeaderTitle
+                ? firstHeaderTitle.text
+                : 'No Title';
+
+        const stacks = metadata.stacks
+            ? metadata.stacks
+            : [];
+
+        const createdDatetime = !metadata.date
+            ? null
+            : !Number.isNaN(Date.parse(metadata.date))
+                ? Date.parse(metadata.date)
+                : null;
+
+        return {
+            title,
+            snippets: [...snippets],
+            stackIds: stacks,
+            createdDatetime,
+        };
+    }
+
+    // TODO: Test and implement this method.
+    makeContentToRawValue(content: NoteContent): string {
+        return null;
+    }
+}
