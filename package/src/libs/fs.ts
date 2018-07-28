214d12967a6539bd91e65cb278f010365a95eb48
seokju-na <seokju.me@gmail.com>
2018-07-28 17:50:52 +0900

    Remove unused functions

--- package/src/libs/fs.ts
+++ package/src/libs/fs.ts
@@ -7,28 +7,18 @@ import { promisify } from 'util';
 export const accessAsObservable = bindNodeCallback(fs.access);
 export const accessAsPromise = promisify(fs.access);
 
-export async function isDirectoryExists(dirname: string): Promise<boolean> {
-    try {
-        await accessAsPromise(dirname);
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
 
 export const readFileAsObservable = bindNodeCallback((
     filename: string,
     encoding: string,
     callback: (error: Error, buffer: Buffer) => void,
 ) => fs.readFile(filename, encoding, callback));
-export const readFileAsPromise = promisify(fs.readFile);
 
 
 export const readdirAsObservable = bindNodeCallback((
     dirname: string,
     callback: (error: Error, files: string[]) => void,
 ) => fs.readdir(dirname, callback));
-export const readdirAsPromise = promisify(fs.readdir);
 
 
 export const writeFileAsObservable = bindNodeCallback((
@@ -37,15 +27,10 @@ export const writeFileAsObservable = bindNodeCallback((
     encoding: string,
     callback: (error: Error) => void,
 ) => fs.writeFile(filename, value, callback));
-export const writeFileAsPromise = promisify(fs.writeFile);
 
 
 export const mkdirAsObservable = bindNodeCallback(fs.mkdir);
 export const mkdirAsPromise = promisify(fs.mkdir);
-
-
-export const rmdirAsObservable = bindNodeCallback(fs.rmdir);
-export const rmdirAsPromise = promisify(fs.rmdir);
 
 
 export function ensureDirAsObservable(dirname: string): Observable<void> {
