ca2259331601a5c27b8387005fd4534bf969f2ba
seokju-na <seokju.me@gmail.com>
2018-08-02 23:44:23 +0900

    Update note editor to render snippet editors

--- package/src/browser/note/note-editor/note-editor.component.ts
+++ package/src/browser/note/note-editor/note-editor.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NoteSnippetTypes } from '../../../models/note-snippet';
 import { WorkspaceService } from '../../core/workspace.service';
+import { NoteSnippetEditor } from '../note-snippet-editors/note-snippet-editor';
 import { NoteCollectionService } from '../shared/note-collection.service';
+import { NoteSnippetContent } from '../shared/note-content.model';
+import { NoteEditorService, NoteSnippetEditorOutlet } from '../shared/note-editor.service';
 
 
 @Component({
@@ -8,16 +13,40 @@ import { NoteCollectionService } from '../shared/note-collection.service';
     templateUrl: './note-editor.component.html',
     styleUrls: ['./note-editor.component.scss'],
 })
-export class NoteEditorComponent implements OnInit {
+export class NoteEditorComponent implements OnInit, AfterViewInit {
+    readonly content1: NoteSnippetContent = {
+        type: NoteSnippetTypes.TEXT,
+        value: 'Hello World!',
+    };
+
+    readonly content2: NoteSnippetContent = {
+        type: NoteSnippetTypes.CODE,
+        value: 'function helloWorld(): void;',
+        codeLanguageId: 'typescript',
+        codeFileName: 'abc.ts',
+    };
+
+    @ViewChildren('snippet', { read: NoteSnippetEditor })
+    snippetEditors: QueryList<NoteSnippetEditor>;
+
+    readonly snippetTypes = NoteSnippetTypes;
+
+    readonly snippetOutlets: Observable<NoteSnippetEditorOutlet[]> =
+        this.editor.getOutlets();
 
     constructor(
         private workspace: WorkspaceService,
+        private editor: NoteEditorService,
         private collection: NoteCollectionService,
     ) {
     }
 
     ngOnInit(): void {
         this.collection.load();
+    }
+
+    ngAfterViewInit(): void {
+        this.editor.initSnippetEditors(this.snippetEditors);
     }
 
 }
