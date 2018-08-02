993378b74fb0d5fa3cf12687f17d08ef19ec5dd9
seokju-na <seokju.me@gmail.com>
2018-08-02 23:40:07 +0900

    Fix type

--- package/test/helpers/type-in-element.ts
+++ package/test/helpers/type-in-element.ts
@@ -1,7 +1,10 @@
 import { dispatchFakeEvent } from './dispatch-event';
 
 
-export function typeInElement(value: string, element: HTMLInputElement) {
+export function typeInElement(
+    value: string,
+    element: HTMLInputElement | HTMLTextAreaElement,
+): void {
     element.focus();
     element.value = value;
     dispatchFakeEvent(element, 'input');
