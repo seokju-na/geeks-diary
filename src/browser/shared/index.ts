de339cf3408c904067896892caae92d81a7960cd
seokju-na <seokju.me@gmail.com>
2018-09-30 21:19:06 +0900

    Add git service

--- src/browser/shared/index.ts
+++ src/browser/shared/index.ts
@@ -3,3 +3,4 @@ export * from './shared.module';
 export * from './workspace-database';
 export * from './workspace.service';
 export * from './authentication-database';
+export * from './git.service';
