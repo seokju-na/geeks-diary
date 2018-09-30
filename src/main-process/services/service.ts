656a5ba5f5bd6885cb31b0de7c0b304e20eb8636
seokju-na <seokju.me@gmail.com>
2018-09-30 22:43:32 +0900

    Add ipc server integration

--- src/main-process/services/service.ts
+++ src/main-process/services/service.ts
@@ -1,9 +1,28 @@
 import { EventEmitter } from 'events';
+import { PROP_METADATA } from '../../libs/decorators';
+import { IpcServer } from '../../libs/ipc';
 
 
 export abstract class Service extends EventEmitter {
+    protected ipc: IpcServer;
+
     protected constructor(public readonly name: string) {
         super();
+
+        this.ipc = new IpcServer(name);
+        this.ipc.setErrorHandler(error => this.handleError(error));
+
+        // Assign actions handlers
+        const actionHandlers = this.constructor[PROP_METADATA];
+
+        if (actionHandlers) {
+            for (const actionName of Object.keys(actionHandlers)) {
+                const action = actionHandlers[actionName][0].action;
+                const method = this[actionName];
+
+                this.ipc.registerActionHandler(action, method.bind(this));
+            }
+        }
     }
 
     abstract init(...di: Service[]): void | Promise<void>;
