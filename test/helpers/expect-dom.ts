7d21ea2894475c163d87d4fea6e48a2a559ca085
seokju-na <seokju.me@gmail.com>
2018-10-03 18:58:15 +0900

    Add expect dom utility

---
+++ test/helpers/expect-dom.ts
@@ -0,0 +1,54 @@
+class DomExpectMatcher {
+    readonly not: DomExpectMatcher;
+
+    constructor(private element: HTMLElement, private denied: boolean = false) {
+        if (!this.denied) {
+            this.not = new DomExpectMatcher(element, true);
+        }
+    }
+
+    toContainClasses(...classNames: string[]): any {
+        let allClassesContained = true;
+
+        for (const className of classNames) {
+            if (!this.element.classList.contains(className)) {
+                allClassesContained = false;
+            }
+        }
+
+        return expect(allClassesContained).toBe(!this.denied);
+    }
+
+    toContainText(text: string): any {
+        if (this.denied) {
+            return expect(this.element.innerText).not.toContain(text);
+        } else {
+            return expect(this.element.innerText).toContain(text);
+        }
+    }
+
+    toBeDisabled(): any {
+        return expect((this.element as any).disabled).toBe(!this.denied);
+    }
+
+    toHaveAttribute(name: string, value: string): any {
+        if (this.denied) {
+            return expect(this.element.getAttribute(name)).not.toEqual(value);
+        } else {
+            return expect(this.element.getAttribute(name)).toEqual(value);
+        }
+    }
+
+    toBeFocused(): any {
+        if (this.denied) {
+            return expect(document.activeElement).not.toEqual(this.element);
+        } else {
+            return expect(document.activeElement).toEqual(this.element);
+        }
+    }
+}
+
+
+export function expectDom(element: HTMLElement): DomExpectMatcher {
+    return new DomExpectMatcher(element);
+}
