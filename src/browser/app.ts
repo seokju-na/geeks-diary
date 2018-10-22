dfbb19785d9f28b084835242f5e37d6d155aaceb
seokju-na <seokju.me@gmail.com>
2018-10-22 14:36:42 +0900

    Add workspace database initialize step before app module boostraps

--- src/browser/app.ts
+++ src/browser/app.ts
@@ -2,12 +2,14 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from '../core/environment';
 import { AppModule } from './app/app.module';
+import { workspaceDatabase } from './shared';
 
 
 if (environment.production) {
     enableProdMode();
 }
 
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
+
+Promise.all([workspaceDatabase.init()])
+    .then(() => platformBrowserDynamic().bootstrapModule(AppModule))
     .catch(error => console.error(error));
