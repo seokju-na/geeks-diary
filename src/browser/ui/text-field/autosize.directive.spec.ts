2bf3a96e8ebd8e43dcf12b8f6ec82d729df1e68c
seokju-na <seokju.me@gmail.com>
2018-10-22 14:36:50 +0900

    Remove unused spec

--- src/browser/ui/text-field/autosize.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { AutosizeDirective } from './autosize.directive';
-//
-// describe('AutosizeDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new AutosizeDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
