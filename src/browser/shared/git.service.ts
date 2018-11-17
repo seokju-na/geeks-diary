f2fa1ee24950190319b5ab0c95362d3eba54d7ea
seokju-na <seokju.me@gmail.com>
2018-11-17 17:50:34 +0900

    Add method for fetch file changes

--- src/browser/shared/git.service.ts
+++ src/browser/shared/git.service.ts
@@ -1,16 +1,27 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable, NgZone, OnDestroy } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { GitCloneOptions } from '../../core/git';
-import { VcsAuthenticationInfo } from '../../core/vcs';
+import { VcsAuthenticationInfo, VcsFileChange } from '../../core/vcs';
 import { IpcActionClient } from '../../libs/ipc';
+import { enterZone } from '../../libs/rx';
 
 
 @Injectable()
 export class GitService implements OnDestroy {
     private ipcClient = new IpcActionClient('git');
 
+    constructor(private ngZone: NgZone) {
+    }
+
     ngOnDestroy(): void {
         this.ipcClient.destroy();
+    }
+
+    getFileChanges(dirPath: string): Observable<VcsFileChange[]> {
+        return from(this.ipcClient.performAction<string, VcsFileChange[]>(
+            'getFileChanges',
+            dirPath,
+        )).pipe(enterZone(this.ngZone));
     }
 
     cloneRepository(
