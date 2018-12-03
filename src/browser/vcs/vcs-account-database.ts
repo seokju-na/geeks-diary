66286d7c54ea495957e01a07c1f098c3cd12a2b9
seokju-na <seokju.me@gmail.com>
2018-12-03 21:05:12 +0900

    Add vcs account metadata table

--- src/browser/vcs/vcs-account-database.ts
+++ src/browser/vcs/vcs-account-database.ts
@@ -4,14 +4,25 @@ import { Database } from '../../core/database';
 import { VcsAccount } from '../../core/vcs';
 
 
+export interface VcsAccountMetadataMap {
+    accountEmail: string;
+    isRepositoryFetchAccount?: number;
+}
+
+
+const TRUE = 1;
+
+
 export class VcsAccountDatabase extends Database {
     readonly accounts!: Dexie.Table<VcsAccount, number>;
+    readonly metadata!: Dexie.Table<VcsAccountMetadataMap, string>;
 
     constructor() {
         super('VcsAccount');
 
         this.conditionalVersion(1, {
             accounts: '++, name, email, authentication',
+            metadata: 'accountEmail, isRepositoryFetchAccount',
         });
     }
 
@@ -42,6 +53,31 @@ export class VcsAccountDatabase extends Database {
     async isAccountExists(email: string): Promise<boolean> {
         const count = await this.accounts.where({ email }).count();
         return count > 0;
+    }
+
+    async getRepositoryFetchAccount(): Promise<VcsAccount | undefined> {
+        const metadata = await this.metadata.get({ isRepositoryFetchAccount: TRUE });
+
+        if (!metadata) {
+            return undefined;
+        }
+
+        return await this.accounts.get({ email: metadata.accountEmail });
+    }
+
+    async setRepositoryFetchAccountAs(account: VcsAccount): Promise<void> {
+        const previousKeys = await this.metadata
+            .where({ isRepositoryFetchAccount: TRUE })
+            .primaryKeys();
+
+        if (previousKeys.length > 0) {
+            await this.metadata.bulkDelete(previousKeys);
+        }
+
+        await this.metadata.put({
+            accountEmail: account.email,
+            isRepositoryFetchAccount: TRUE,
+        });
     }
 }
 
