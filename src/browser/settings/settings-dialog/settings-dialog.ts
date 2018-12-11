073b7074a5da2b70b4afb83e2bf0f7df93e3bed6
seokju-na <seokju.me@gmail.com>
2018-12-12 01:02:44 +0900

    Update only one settings dialog can be opened

--- src/browser/settings/settings-dialog/settings-dialog.ts
+++ src/browser/settings/settings-dialog/settings-dialog.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Dialog, DialogRef } from '../../ui/dialog';
 import { SettingsDialogData } from './settings-dialog-data';
 import { SettingsDialogComponent } from './settings-dialog.component';
 
 
 @Injectable()
-export class SettingsDialog {
+export class SettingsDialog implements OnDestroy {
+    private dialogRef: DialogRef<SettingsDialogComponent, void> | null = null;
+    private dialogCloseSubscription = Subscription.EMPTY;
+
     constructor(private dialog: Dialog) {
     }
 
+    ngOnDestroy(): void {
+        this.dialogCloseSubscription.unsubscribe();
+    }
+
     open(data?: SettingsDialogData): DialogRef<SettingsDialogComponent, void> {
-        return this.dialog.open<SettingsDialogComponent,
+        if (this.dialogRef) {
+            return this.dialogRef;
+        }
+
+        this.dialogRef = this.dialog.open<SettingsDialogComponent,
             SettingsDialogData,
             void>(
             SettingsDialogComponent,
@@ -21,5 +33,16 @@ export class SettingsDialog {
                 data,
             },
         );
+
+        // If subscription exists, unsubscribe first.
+        if (this.dialogCloseSubscription) {
+            this.dialogCloseSubscription.unsubscribe();
+        }
+
+        this.dialogCloseSubscription = this.dialogRef.afterClosed().subscribe(() => {
+            this.dialogRef = null;
+        });
+
+        return this.dialogRef;
     }
 }
