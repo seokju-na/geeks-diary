5711793fb29f930399f71412047b28d01b42b038
seokju-na <seokju.me@gmail.com>
2018-12-13 23:48:50 +0900

    #18 Add note context menu

---
+++ src/browser/note/note-collection/note-item/note-item-context-menu.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MenuItemConstructorOptions } from 'electron';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { commonMenuLabels, NativeMenu } from '../../../ui/menu';
+
+
+export type NoteItemContextMenuCommand = 'revealInFinder';
+
+
+@Injectable()
+export class NoteItemContextMenu {
+    constructor(private nativeMenu: NativeMenu) {
+    }
+
+    open(): Observable<NoteItemContextMenuCommand | undefined> {
+        return this.nativeMenu.open(this.buildTemplate()).afterClosed().pipe(
+            map(item => item ? item.id as NoteItemContextMenuCommand : undefined),
+        );
+    }
+
+    private buildTemplate(): MenuItemConstructorOptions[] {
+        return [
+            {
+                id: 'revealInFinder',
+                label: commonMenuLabels.revealInFileManager,
+            },
+        ];
+    }
+}
