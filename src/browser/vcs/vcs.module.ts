e907446c89c6050460727028f805f1efeea119fa
seokju-na <seokju.me@gmail.com>
2018-12-21 17:02:31 +0900

    Export missing VcsLocalModule

--- src/browser/vcs/vcs.module.ts
+++ src/browser/vcs/vcs.module.ts
@@ -36,6 +36,7 @@ import { VcsService } from './vcs.service';
     exports: [
         VcsRemoteModule,
         VcsViewModule,
+        VcsLocalModule,
         VcsSettingsModule,
         VcsManagerComponent,
     ],
