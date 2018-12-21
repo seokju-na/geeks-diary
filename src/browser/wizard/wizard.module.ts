f69d70e97a1ba839cc52aa933fc6b34eb81f2bb6
seokju-na <seokju.me@gmail.com>
2018-12-21 14:46:41 +0900

    Fix static injection error

--- src/browser/wizard/wizard.module.ts
+++ src/browser/wizard/wizard.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { combineReducers, StoreModule } from '@ngrx/store';
 import { SharedModule } from '../shared';
 import { UiModule } from '../ui/ui.module';
 import { VcsModule } from '../vcs';
+import { WizardVcsAuthenticationInfoExistsResolver } from './resolvers';
 import { WizardChoosingModule } from './wizard-choosing';
 import { WizardCloningModule } from './wizard-cloning';
 import { WizardRoutingModule } from './wizard-routing.module';
@@ -15,6 +18,8 @@ import { WizardComponent } from './wizard.component';
         BrowserModule,
         BrowserAnimationsModule,
         UiModule,
+        StoreModule.forRoot(combineReducers({})),
+        EffectsModule.forRoot([]),
         SharedModule,
         VcsModule,
         WizardChoosingModule,
@@ -22,6 +27,9 @@ import { WizardComponent } from './wizard.component';
         WizardRoutingModule,
     ],
     declarations: [WizardComponent],
+    providers: [
+        WizardVcsAuthenticationInfoExistsResolver,
+    ],
     bootstrap: [WizardComponent],
 })
 export class WizardModule {
